7e20da345c33cda611080f464458e054f3835fd3
Kerem Toplu <61215181+keremtoplu@users.noreply.github.com>
2023-02-16 22:42:34 +0300

    Reciever that the user can add in-game has been added..The receiver registers and automatically appears on the map when you turn it off and on. And it registers in sql database.

--- MineSimilator/Assets/Scripts/AddReciever.cs
+++ MineSimilator/Assets/Scripts/AddReciever.cs
@@ -23,37 +23,70 @@ public class Receiver
 }
 public class AddReciever : MonoBehaviour
 {
-    [SerializeField]
-    private List<Receiver> _recieverList=new List<Receiver>();
+    // [SerializeField]
+    // private List<Receiver> _recieverList=new List<Receiver>();
 
-    [SerializeField]
-    private List<Receiver> _recieverList1=new List<Receiver>();
+    // [SerializeField]
+    // private List<Receiver> _recieverList1=new List<Receiver>();
 
-    [SerializeField]
-    private List<Receiver> _recieverList2=new List<Receiver>();
+    // [SerializeField]
+    // private List<Receiver> _recieverList2=new List<Receiver>();
     
+    [SerializeField]
+    private GameObject recieverPref;
 
-    public void _JsonSave()
+    [SerializeField]
+    private GameObject recieversParent;
+
+    // public void _JsonSave()
+    // {
+
+    //     MinerList miner=new MinerList();
+    //     RecieverList recieverList=new RecieverList();
+    //     RecieverList recieverList2=new RecieverList();
+    //     RecieverList recieverList3=new RecieverList();
+        
+    //     for (int i = 0; i < _recieverList.Count; i++)
+    //     {
+    //         recieverList.recieverList.Add(_recieverList[i]);
+    //         recieverList2.recieverList.Add(_recieverList1[i]);
+    //         recieverList3.recieverList.Add(_recieverList2[i]);
+    //     }
+
+    //     miner.minerRecieverList.Add(recieverList);
+    //     miner.minerRecieverList.Add(recieverList2);
+    //     miner.minerRecieverList.Add(recieverList3);
+        
+    //     string convertJson=JsonUtility.ToJson(miner);
+    //     System.IO.File.WriteAllText(Application.persistentDataPath+"/RecieverList.json",convertJson);
+    // }
+
+
+
+    private void Start() 
     {
-
-        MinerList miner=new MinerList();
-        RecieverList recieverList=new RecieverList();
-        RecieverList recieverList2=new RecieverList();
-        RecieverList recieverList3=new RecieverList();
         
-        for (int i = 0; i < _recieverList.Count; i++)
+        for (int i = 1; i <= PlayerPrefs.GetInt("currentSaveReciever"); i++)
         {
-            recieverList.recieverList.Add(_recieverList[i]);
-            recieverList2.recieverList.Add(_recieverList1[i]);
-            recieverList3.recieverList.Add(_recieverList2[i]);
-        }
+            var x=PlayerPrefs.GetFloat("posx"+i);
+            var y=PlayerPrefs.GetFloat("posy"+i);
+            var z=PlayerPrefs.GetFloat("posz"+i);
+            var desPos= new Vector3(x,y,z);
 
-        miner.minerRecieverList.Add(recieverList);
-        miner.minerRecieverList.Add(recieverList2);
-        miner.minerRecieverList.Add(recieverList3);
-        
-        string convertJson=JsonUtility.ToJson(miner);
-        System.IO.File.WriteAllText(Application.persistentDataPath+"/RecieverList.json",convertJson);
+           var newReciever= Instantiate(recieverPref,desPos,Quaternion.identity,recieversParent.transform);
+           newReciever.GetComponent<Reciever>().IsBreak=true;
+        }    
     }
+
+    //anten ekleme
+
+    public void ProduceReciever()
+    {
+        Instantiate(recieverPref);
+        PlayerPrefs.SetInt("currentSaveReciever",PlayerPrefs.GetInt("currentSaveReciever")+1);
+
+    }
+
+
 
 }
---
+++ MineSimilator/Assets/Scripts/Reciever.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reciever : MonoBehaviour
+{
+   
+    private RaycastHit hit;
+    private Vector3 movePoint;
+
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    private bool isBreak=false;
+
+    private int currentSaveReciever;
+
+    public bool IsBreak { get
+    {
+        return isBreak;
+    }
+    set
+    {
+        isBreak=value;
+
+    } }
+    
+    private void Update() 
+    {
+        if(isBreak==false)
+        {
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+            if(Physics.Raycast(ray,out hit,1000.0f,layerMask))
+            {
+                transform.position=hit.point;
+                
+            }
+            if(Input.GetMouseButtonDown(0))
+            {
+                isBreak=true;
+                PlayerPrefs.SetFloat("posx"+PlayerPrefs.GetInt("currentSaveReciever"),transform.position.x);
+                PlayerPrefs.SetFloat("posy"+PlayerPrefs.GetInt("currentSaveReciever"),transform.position.y);
+                PlayerPrefs.SetFloat("posz"+PlayerPrefs.GetInt("currentSaveReciever"),transform.position.z);
+                //kayıt burda bıraktığımız an çalışacak
+                SaveRecieverDataBase();
+            }
+        }
+        
+        Debug.Log(PlayerPrefs.GetInt("currentSaveReciever"));
+    }
+
+
+    private void SaveRecieverDataBase()
+    {
+        //buraya eklenen antenleri veritabanına kayıtını yazacaksın.
+    }
+   
+}
